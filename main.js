3c3e2475bfbfb8f259f3aaff19486663ae77a8d8
debasish-itobuz <debasish.das@itobuz.com>
2024-03-14 18:43:17 +0530

    formatted

--- main.js
+++ main.js
@@ -6,23 +6,13 @@ const board = document.querySelector(".board");
 let round = 0;
 let inputCount = 0;
 let currentWord = "";
-// console.log(words)
 
 keyBoard.addEventListener("click", (e) => {
-    // console.log(e.target.innerText)
-    // console.log(board.children[round])
-
     const activeRow = board.children[round];
-    // console.log(activeRow);
-    // console.log(e.target.innerText);
-    // console.log(e.target.innerText.length);
 
     if (e.target.innerText.length === 1 && round <= 5) {
         activeRow.children[inputCount].innerHTML = e.target.innerText;
         currentWord += e.target.innerText;
-
-        // console.log(currentWord);
-        // console.log(activeRow.children[inputCount])
         inputCount++;
         if (inputCount === 5) {
             const data = words.find((item) => {
